0acc91e33fd2bf3d843ca5a1040bf29e3b771eb5
VIVIAN-1985 <vivianvince@gmail.com>
2024-12-08 18:39:10 +0530

    Add files via upload

---
+++ pavandemoday2/ifconditiondemo.java
@@ -0,0 +1,91 @@
+package pavandemoday2;
+
+public class ifconditiondemo {
+
+	public static void main(String[] args)
+	
+	{
+	/*
+		int person_age=15;
+		
+		if(person_age>=10)
+		{
+			System.out.println("elgible for vote");
+			
+		}
+		*/
+		
+/*
+		int person_age=15;
+		
+		if(person_age>=18)
+		{
+			System.out.println("elegible for vote");
+		}
+			else 
+			
+			{
+					System.out.println("not elegible for vote");
+			}
+		
+
+}
+	}
+*/
+		/*
+		{
+		int number=15;
+			if (number%2==0)
+			{
+				System.out.println("even number");
+		}
+			else
+			{
+				System.out.println("odd number");
+			}
+			} 
+		*/
+	
+		/*
+		int week =8;
+		if (week==1)
+			{System.out.println("sunday");}
+		else if (week==2)
+			{System.out.println("monday");}
+		else if (week==3)
+			{System.out.println("tuesday");}
+		else if (week==4)
+			{System.out.println("wednesday");}
+		else if (week==5)
+			{System.out.println("thrusday");}
+		else if (week==6)
+			{System.out.println("friday");}
+		else if (week==7)
+		{	System.out.println("saturday");}
+		else {System.out.println("invalid week numbers");}
+		*/
+	
+	//Switch case
+		
+		int weekno=3;
+		
+		switch(weekno)
+		{
+		case 1: System.out.println("monday"); break;
+		case 2: System.out.println("tuesday"); break;
+		case 3: System.out.println("wednesday"); break;
+		case 4: System.out.println("thrusday"); break;
+		case 5: System.out.println("friday"); break;
+		case 6: System.out.println("saturday"); break;
+		case 7: System.out.println("sunday"); break;
+		default : System.out.println("invalid week number");
+		}
+	}
+}	
+		
+
+
+
+	
+	
+	
