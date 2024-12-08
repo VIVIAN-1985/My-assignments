43d2776a1e8f0bdc08bb7fd2f3fd6dd768917da3
VIVIAN-1985 <vivianvince@gmail.com>
2024-12-08 18:09:42 +0530

    Add files via upload

---
+++ day1/Firefox.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Firefox {
+public static void main(String[] args) {
+
+	
+	float  browserversion = 100.2f;
+	String  browsername = "firefox";
+	boolean isvariable = true;
+	int releaseyear = 1998;
+	char browserLogo = 'f';
+	
+	System.out.println("Browserversion : "+browserversion);
+	System.out.println("Browsername : "+browsername);
+	System.out.println("Isvariable : "+isvariable);
+	System.out.println("Releaseyear : "+releaseyear);
+	System.out.println("Browserlogo : "+browserLogo);
+	
+}
+}
+
+
+// float browserversion = 100.2f;
+// string browsername = "firefox";
+// boolean is visible = true;
+// int release year = 1998;
+// char browserlogo = 'f';
+
+
---
+++ day1/Firstcode.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Firstcode {
+	
+public static void main(String[] args) {
+	
+	System.out.println("Hello testleaf");
+	
+
+	
+	
+}
+}
